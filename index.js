e31da0703ecc5ed47ba36991eace46d177c40188
shweta-dubey <shwetadubeydev@gmail.com>
2018-06-06 12:48:56 +0530

    get pusherConfig online

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Pusher = require('pusher-js/node');
 const axios = require('axios');
-const pusherConfig = require('./pusher.json');
+// const pusherConfig = require('./pusher.json');
+var pusherConfig;
+var user='u112';
 
 var arr=[
     {key: 1, value: true},
@@ -8,17 +10,24 @@ var arr=[
     {key: 3, value: true},
     {key: 4, value: true}];
 
-// Pass Data on Server.
-axios.post(`${pusherConfig.restServer}/api/${pusherConfig.user}/assign`, arr);
+
+axios.post('https://vishnumavawala.000webhostapp.com/Home/pusher.php', { 'device': 'Pi' })
+    .then((res) => {
+
+        pusherConfig=res.data;
+        console.log('Connection Establishment');
+        // Pass Data on Server.
+        axios.post(`${pusherConfig.restServer}/api/${user}/assign`, arr);
 
 
-// Client which handle response to server
-var pusher = new Pusher(pusherConfig.key, {
-    cluster: pusherConfig.cluster,
-    encrypted: true
-});
+        // Client which handle response to server
+        var pusher = new Pusher(pusherConfig.key, {
+            cluster: pusherConfig.cluster,
+            encrypted: true
+        });
 
-var channel = pusher.subscribe(pusherConfig.user);
-channel.bind('part', (data) => {
-    console.log(data);
-});
+        var channel = pusher.subscribe(user);
+        channel.bind('part', (data) => {
+            console.log(data);
+        });
+    });
