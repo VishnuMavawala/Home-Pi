fcc6083b4c5738a4c5a97d2f5e20f78c05372925
Vishnu <lanetteam.vishnu@gmail.com>
2018-06-08 23:12:20 +0530

    call Server Api

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const Pusher = require('pusher-js/node');
 const axios = require('axios');
 // const pusherConfig = require('./pusher.json');
@@ -10,15 +12,30 @@ var arr=[
     {key: 3, value: true},
     {key: 4, value: true}];
 
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/api/:arduino/arduinoInit', function(req, res) {
+    console.log('User joined: ', req.params.arduino);
+    axios.get(`${pusherConfig.restServer}/api/${user}/init`);
+    res.send(arr);
+});
+app.post('/api/:arduino/part', function(req, res) {
+    console.log('User part: ', req.body);
+    arr=req.body;
+    axios.post(`${pusherConfig.restServer}/api/${user}/part`, arr);
+    res.sendStatus(204);
+});
+app.listen(4000, function() {
+    console.log('App listening on port 4000');
+});
 
 axios.post('https://vishnumavawala.000webhostapp.com/Home/pusher.php', { 'device': 'Pi' })
     .then((res) => {
-
         pusherConfig=res.data;
         console.log('Connection Establishment');
         // Pass Data on Server.
         axios.post(`${pusherConfig.restServer}/api/${user}/assign`, arr);
-
 
         // Client which handle response to server
         var pusher = new Pusher(pusherConfig.key, {
@@ -29,5 +46,6 @@ axios.post('https://vishnumavawala.000webhostapp.com/Home/pusher.php', { 'device
         var channel = pusher.subscribe(user);
         channel.bind('part', (data) => {
             console.log(data);
+            axios.post('http://192.168.0.103:80/body', data);
         });
     });
